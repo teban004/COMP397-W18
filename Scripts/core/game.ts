6fc4c204408331990516c87f19a7a801294ab4cd
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2018-02-11 13:05:43 -0500

    added typescript and createjs support

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,31 +1,42 @@
 // iIFE - Immediately Invoked Function Expression
-(function () {
+(function(){
+
     // Game Variables
-    var canvas = document.getElementById("canvas");
-    var stage;
-    var helloLabel;
-    function Init() {
+    let canvas = document.getElementById("canvas");
+    let stage: createjs.Stage;
+    let helloLabel: createjs.Text;
+
+    function Init():void {
         console.log("Initialization Started");
+
         Start();
     }
-    function Start() {
+
+    function Start():void {
         console.log("Starting Application...");
+
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60; // 60 FPS
         createjs.Ticker.on("tick", Update);
         Main();
     }
-    function Update() {
+
+    function Update():void {
         helloLabel.rotation += 5;
         stage.update(); // redraws the stage
     }
-    function Main() {
+
+    function Main():void {
         console.log("Game Started...");
+
         helloLabel = new createjs.Text("Hell, world!", "40px Consolas", "#000000");
         helloLabel.x = 100;
         helloLabel.y = 100;
+
         stage.addChild(helloLabel);
+
     }
+
     window.onload = Init;
+
 })();
-//# sourceMappingURL=game.js.map
