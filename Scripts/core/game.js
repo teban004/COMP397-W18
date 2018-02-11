26da027101a8688334d6a2638dd344079671c53c
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2018-02-11 12:28:51 -0500

    added npm support

---
+++ Scripts/core/game.js
@@ -0,0 +1,22 @@
+// iIFE - Immediately Invoked Function Expression
+(function(){
+
+    function Init() {
+        console.log("Initialization Started");
+    }
+
+    function Start() {
+
+    }
+
+    function Update() {
+
+    }
+
+    function Main() {
+
+    }
+
+    window.onload = Init;
+
+})();
